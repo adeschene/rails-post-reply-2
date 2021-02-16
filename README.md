250e25acd018a7734f5dc604839c8609fd426212
adeschene <alecdeschene@gmail.com>
2021-02-15 23:15:32 -0800

    setup pg, devise, avatar

--- README.md
+++ README.md
@@ -1,24 +1,5 @@
-# README
+Tenth Ruby on Rails Bootcamp assignment.
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+A slightly less simple message board type thing.
 
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+Uses bootstrap for styling and devise for user account management.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -117,4 +117,16 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.default_url_options = { host: 'rails-post-reply-2.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    domain:               'gmail.com',
+    address:              "smtp.gmail.com",
+    port:                 587,
+    authentication:       :plain,
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
+    enable_starttls_auto: true
+  }
 end
