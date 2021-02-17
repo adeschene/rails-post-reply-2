e3e4bfac06ebd8a5c3492dfdfc090d6c4dac83a8
adeschene <alecdeschene@gmail.com>
2021-02-17 01:08:40 -0800

    add topic lists to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def home
+    @recent_topics  = Topic.all.order(created_at: :desc).first(10)
+    @user_topics    = current_user.topics.order(created_at: :desc).limit(10)
   end
 end
