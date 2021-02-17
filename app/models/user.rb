c3c6bbcb1b5247b9426acb18687882fedf025eca
adeschene <alecdeschene@gmail.com>
2021-02-17 01:46:01 -0800

    add username with authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,4 +10,12 @@ class User < ApplicationRecord
 
   has_many :topics, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  def self.find_for_database_authentication warden_condition
+    conditions = warden_condition.dup
+    login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", {
+      value: login.strip.downcase
+    }]).first
+  end
 end
